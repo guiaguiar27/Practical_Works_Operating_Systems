5a9514c918cf84831af6c96597a1cb775f271ee5
GabrielMoraisReis <gmoraisreis@gmail.com>
2020-10-31 17:45:23 -0300

    Coloquei PC para continuar de onde parou no caso do block

--- PW1/processo_simulado.py
+++ PW1/processo_simulado.py
@@ -5,9 +5,15 @@ class Processo_simulado:
         self.contador = [] #essa variavel garante que a posição da memória foi alocada e que também teve um valor atribuido a ela
 
 
-    def ler_arquivo_init(self, nome_arquivo):
+    def ler_arquivo_init(self, nome_arquivo, pc_inicial):
         with open(nome_arquivo, "r") as f:
+            count_bloq = 0
             for line in f:
+                count_bloq+=1
+
+                if count_bloq < pc_inicial+1: #garante que o programa volte a executar a partir da instrução que parou
+                    break
+
                 self.pc+=1
                 line = line.split()
                 if line[0] == 'N':
@@ -36,8 +42,12 @@ class Processo_simulado:
                         break   
                 elif line[0] == 'B':
                     print("Bloqueia processo")
+                    #manda o pc pra tabela processos
+                    #na hora que voltar tem que voltar com o nome do arquivo que tava executando e o contador de processos que terminou
+                    #então tem que ter alguma coisa do tipo: return (nome_arquivo, self.pc)
                 elif line[0] == 'T':
                     print("Termina processo")
+                    #matar o processo
                 elif line[0] == 'F':
                     processo = Processo_simulado()
                     processo.ler_arquivo_init()
@@ -51,4 +61,4 @@ class Processo_simulado:
 
 
 a = Processo_simulado() 
-a.ler_arquivo_init("teste_processo.txt")
+a.ler_arquivo_init("teste_processo.txt", 0)
